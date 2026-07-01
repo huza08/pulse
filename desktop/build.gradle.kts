46195dfcdfa210f755c2c1fcc608b15c4b9f980e
huza <huzaifahasif3@gmail.com>
2026-07-01 23:24:46 +0700

    feat(desktop): add all providers as JVM dependencies

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -7,6 +7,14 @@ plugins {
 dependencies {
     implementation(compose.desktop.currentOs)
     implementation(compose.material3)
+
+    implementation(projects.providers.innertube)
+    implementation(projects.providers.kugou)
+    implementation(projects.providers.lrclib)
+    implementation(projects.providers.piped)
+    implementation(projects.providers.github)
+    implementation(projects.providers.sponsorblock)
+    implementation(projects.providers.translate)
 }
 
 compose.desktop {
